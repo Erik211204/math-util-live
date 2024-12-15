5a7cb3dfedc90abd21a8e4845d0883c465623b92
Erik211204 <12b4thanh@gmail.com>
2024-12-15 22:14:29 +0700

    Lan thu hai lam chuyen ay - dua code len Server

--- src/com/fu/mathutil/MathUtilLive.java
+++ src/com/fu/mathutil/MathUtilLive.java
@@ -20,6 +20,8 @@ public class MathUtilLive {
         System.out.println("expected: 5! = 120; actual: " +result);
         System.out.println("expected: 6! = 720; actual: " +MathUtil.getFactorial(6));
         System.out.println("expected: 0! = 1;   actual: " +MathUtil.getFactorial(0));
+        System.out.println("expected: 3! = 6;   actual: " +MathUtil.getFactorial(3));
+        System.out.println("expected: 4! = 24;  actual: " +MathUtil.getFactorial(4));
         // kỹ thuật kiểm thử phần mềm: ước lượng xem giá trị trả về của hàm là gì
         // expected value: 5! = 120
         // mày/hàm ai đó viết khi chạy thực là mấy - actual, giả sử là 120
